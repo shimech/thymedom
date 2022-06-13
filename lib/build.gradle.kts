69abaf7a6c2894c741bdb6f0eea3bfcc634d7501
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-06-13 11:14:45 +0900

    add thymeleaf

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.thymeleaf:thymeleaf:3.0.15.RELEASE")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
     api("org.apache.commons:commons-math3:3.6.1")
